a7922e0bbe9035ac7ed5572d3479355d3f939bfb
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-04-05 20:42:42 -0300

    Fix: Corrige Identação da Query

--- src/main/java/com/example/desafio/api/models/repositorys/CarRepository.java
+++ src/main/java/com/example/desafio/api/models/repositorys/CarRepository.java
@@ -10,16 +10,20 @@ import java.util.List;
 public interface CarRepository extends JpaRepository<CarEntity, Long> {
 
     // query que irá retornar todos os carros + o nome da marca que está associada a ele
-    @Query("SELECT car.id as id, " +
-            "car.timestampRegister as timestampRegister, " +
-            "car.modelId.id as modelId, " +
-            "model.name as modelName, " +
-            "car.year as year, " +
-            "car.fuel as fuel, " +
-            "car.numDoors as numDoors, " +
-            "car.color as color, " +
-            "car.modelId.priceFip as priceFip " +
-            "FROM CarEntity car " +
-            "JOIN car.modelId model")
+    @Query
+            (
+                "SELECT car.id as id, " +
+                "car.timestampRegister as timestampRegister, " +
+                "car.modelId.id as modelId, " +
+                "model.name as modelName, " +
+                "car.year as year, " +
+                "car.fuel as fuel, " +
+                "car.numDoors as numDoors, " +
+                "car.color as color, " +
+                "car.modelId.priceFip as priceFip " +
+                "FROM CarEntity car " +
+                "JOIN car.modelId model"
+            )
+
     List<CarModelProjection> findAllCarsAndModelName();
 }

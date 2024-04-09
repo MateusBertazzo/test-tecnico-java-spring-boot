dfe870e394fd2ca35055f5f67db5bf7e6f874b2d
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-04-09 14:31:51 -0300

    Feat: Adiciona config de CORS para Kotlin

---
+++ src/main/kotlin/com/example/desafio/api/configs/CorsConfig.kt
@@ -0,0 +1,15 @@
+package com.example.desafio.api.configs
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+open class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization")
+    }
+}
